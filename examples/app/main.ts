ed02c34accb2d29772e751edc028803a1f56ad3d
Aden-git <112aden358@gmail.com>
2016-08-15 02:25:02 +0300

    Bootstrap rework to new module syntax

--- examples/app/main.ts
+++ examples/app/main.ts
@@ -1,7 +1,16 @@
-import {bootstrap}    from '@angular/platform-browser-dynamic';
+import {NgModule, enableProdMode} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {FormsModule} from '@angular/forms'
 import {AppComponent} from './app.component';
-import {enableProdMode} from '@angular/core';
 
 enableProdMode();
 
-bootstrap(AppComponent);
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule],
+  bootstrap: [AppComponent],
+})
+class MyAppModule {}
+
+platformBrowserDynamic().bootstrapModule(MyAppModule);
