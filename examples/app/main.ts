7c0aafb94385a2a044face9c1d0bbf4360e50fce
Aden-git <112aden358@gmail.com>
2016-08-15 01:13:11 +0300

    Remove deprecated API

--- examples/app/main.ts
+++ examples/app/main.ts
@@ -1,11 +1,7 @@
 import {bootstrap}    from '@angular/platform-browser-dynamic';
 import {AppComponent} from './app.component';
 import {enableProdMode} from '@angular/core';
-import {disableDeprecatedForms, provideForms} from '@angular/forms';
 
 enableProdMode();
 
-bootstrap(AppComponent, [
-  disableDeprecatedForms(),
-  provideForms(),
-]);
+bootstrap(AppComponent);
