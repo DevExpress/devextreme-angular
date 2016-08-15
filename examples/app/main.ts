c1f5171228dd6a5db341887328c5e71994a7d6cf
Aden-git <112aden358@gmail.com>
2016-08-15 13:35:26 +0300

    Add missing semicolon

--- examples/app/main.ts
+++ examples/app/main.ts
@@ -1,7 +1,7 @@
 import {NgModule, enableProdMode} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {FormsModule} from '@angular/forms'
+import {FormsModule} from '@angular/forms';
 import {AppComponent} from './app.component';
 
 enableProdMode();
