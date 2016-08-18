01c5c358508d3026f6c2b29dddf13ca6f3d7b352
Aden-git <112aden358@gmail.com>
2016-08-18 11:13:56 +0300

    add widget.spec.ts to tslint check

--- tests/src/widget.spec.ts
+++ tests/src/widget.spec.ts
@@ -9,19 +9,80 @@ import {
     EventEmitter,
     ViewChildren,
     NgZone,
-    provide
-} from "@angular/core"
+    provide,
+    Input,
+    Output
+} from '@angular/core';
 
 import {
     inject,
     TestComponentBuilder
-} from "@angular/core/testing";
+} from '@angular/core/testing';
 
 import {
     DxComponent,
     DxTemplateHost,
     DxTemplate
-} from "../../dist";
+} from '../../dist';
+
+// TODO: Try to replace dxButton to Widget ('require' required)
+let dxTestWidget = DevExpress.ui.dxButton['inherit']({
+    NAME: 'dxTestWidget',
+    _render() {
+        this.element()[0].classList.add('dx-test-widget');
+    }
+});
+
+DevExpress.registerComponent('dxTestWidget', dxTestWidget);
+
+@Component({
+    selector: 'dx-test-widget',
+    template: '',
+    providers: [
+        provide(DxTemplateHost, { useClass: DxTemplateHost })
+    ]
+})
+export class DxTestWidget extends DxComponent {
+
+    @Input() testOption: any;
+    @Input() testTemplate: any;
+
+    @Output() onOptionChanged: EventEmitter<any>;
+    @Output() testOptionChange: EventEmitter<any>;
+    @Output() testTemplateChange: EventEmitter<any>;
+
+    constructor(elementRef: ElementRef, ngZone: NgZone, templateHost: DxTemplateHost) {
+        super(elementRef, ngZone, templateHost);
+        this.widgetClassName = 'dxTestWidget';
+        this._events = [
+            { subscribe: 'optionChanged', emit: 'onOptionChanged' },
+            { subscribe: 'initialized', emit: 'onInitialized' }
+        ];
+
+        this._properties = [
+            'testOption',
+            'testTemplate'
+        ];
+
+        this.onOptionChanged = new EventEmitter();
+        this.testOptionChange = new EventEmitter();
+        this.testTemplateChange = new EventEmitter();
+    }
+}
+
+@Component({
+    selector: 'test-container-component',
+    template: '',
+    directives: [DxTestWidget, DxTemplate],
+    queries: {
+        innerWidgets: new ViewChildren(DxTestWidget)
+    }
+})
+export class TestContainerComponent {
+    constructor() {
+    }
+}
+
 
 describe('DevExtreme Angular 2 widget', () => {
     let tcb;
@@ -31,21 +92,21 @@ describe('DevExtreme Angular 2 widget', () => {
     }));
 
     function getWidget(fixture) {
-        var widgetElement = fixture.nativeElement.querySelector(".dx-test-widget") || fixture.nativeElement;
+        let widgetElement = fixture.nativeElement.querySelector('.dx-test-widget') || fixture.nativeElement;
         return dxTestWidget.getInstance(widgetElement);
     }
 
-    //specs
+    // specs
     it('should be rendered', done => {
        tcb
-       .overrideTemplate(TestContainerComponent, '<dx-test-widget [testOption]="\'Test Value\'"></dx-test-widget>')
+       .overrideTemplate(TestContainerComponent, '<dx-test-widget [testOption]="\'Test Value\'" > </dx-test-widget>')
        .createAsync(TestContainerComponent)
             .then(fixture => {
                 fixture.detectChanges();
 
                 let element = getWidget(fixture).element().get(0);
 
-                expect(element.classList).toContain("dx-test-widget");
+                expect(element.classList).toContain('dx-test-widget');
 
                 done();
             })
@@ -54,7 +115,7 @@ describe('DevExtreme Angular 2 widget', () => {
 
     it('should set testOption value to insatnce', done => {
        tcb
-       .overrideTemplate(TestContainerComponent, '<dx-test-widget [testOption]="\'Test Value\'"></dx-test-widget>')
+       .overrideTemplate(TestContainerComponent, '<dx-test-widget [testOption]="\'Test Value\'" > </dx-test-widget>')
        .createAsync(TestContainerComponent)
             .then(fixture => {
                 fixture.detectChanges();
@@ -63,7 +124,7 @@ describe('DevExtreme Angular 2 widget', () => {
                     innerComponent = outerComponent.innerWidgets.toArray()[0],
                     instance = getWidget(fixture);
 
-                expect(instance.option("testOption")).toBe('Test Value');
+                expect(instance.option('testOption')).toBe('Test Value');
                 expect(innerComponent.testOption).toBe('Test Value');
 
                 done();
@@ -79,7 +140,7 @@ describe('DevExtreme Angular 2 widget', () => {
                 let component =  fixture.componentInstance,
                     instance = getWidget(fixture);
 
-                instance.option("testOption", "Changed");
+                instance.option('testOption', 'Changed');
                 expect(component.testOption).toBe('Changed');
 
                 done();
@@ -94,12 +155,12 @@ describe('DevExtreme Angular 2 widget', () => {
             .then(fixture => {
                 fixture.detectChanges();
 
-                var testComponent =  fixture.componentInstance,
+                let testComponent =  fixture.componentInstance,
                     instance = getWidget(fixture);
 
-                testComponent.testOption = "Changed 2";
+                testComponent.testOption = 'Changed 2';
                 fixture.detectChanges();
-                expect(instance.option("testOption")).toBe('Changed 2');
+                expect(instance.option('testOption')).toBe('Changed 2');
                 done();
             })
             .catch(e => done.fail(e));
@@ -116,12 +177,10 @@ describe('DevExtreme Angular 2 widget', () => {
             .then(fixture => {
                 fixture.detectChanges();
 
-                let testComponent = fixture.componentInstance,
-                    innerComponent = testComponent.innerWidgets.toArray()[0],
-                    instance = getWidget(fixture);
+                let instance = getWidget(fixture);
 
-                expect(instance.option("testTemplate")).not.toBeUndefined;
-                expect(typeof instance.option("testTemplate")).toBe("function");
+                expect(instance.option('testTemplate')).not.toBeUndefined;
+                expect(typeof instance.option('testTemplate')).toBe('function');
 
                 done();
             })
@@ -139,23 +198,21 @@ describe('DevExtreme Angular 2 widget', () => {
             .then(fixture => {
                 fixture.detectChanges();
 
-                let testComponent = fixture.componentInstance,
-                    innerComponent = testComponent.innerWidgets.toArray()[0],
-                    instance = getWidget(fixture),
-                    templatesHash = instance.option("_templates");
+                let instance = getWidget(fixture),
+                    templatesHash = instance.option('_templates');
 
-                expect(templatesHash["testTemplate"]).not.toBeUndefined;
-                expect(typeof templatesHash["testTemplate"].render).toBe("function");
+                expect(templatesHash['testTemplate']).not.toBeUndefined;
+                expect(typeof templatesHash['testTemplate'].render).toBe('function');
 
                 done();
             })
             .catch(e => done.fail(e));
     });
 
-    /*  
+    /*
         TODO
         Interpolation doesn't work in the test for unclear reason if we specify it as follows:
-        <div *dxTemplate="let d = data of 'testTemplate'">Template content {{d}}</div>
+        <div *dxTemplate='let d = data of 'testTemplate''>Template content {{d}}</div>
     */
     it('should nonrmalize template function arguments order (#17)', done => {
        tcb
@@ -170,20 +227,19 @@ describe('DevExtreme Angular 2 widget', () => {
 
                 let testComponent = fixture.componentInstance,
                     innerComponent = testComponent.innerWidgets.toArray()[0],
-                    instance = getWidget(fixture),
                     template = innerComponent.testTemplate,
-                    $container = $("<div>");
+                    $container = $('<div>');
 
                 expect(template).not.toBeUndefined;
 
-                template($container); 
-                expect($container.text()).toBe("Template content");
+                template($container);
+                expect($container.text()).toBe('Template content');
 
-                template("test", $container);
-                expect($container.text()).toBe("Template content");
+                template('test', $container);
+                expect($container.text()).toBe('Template content');
 
-                template($container, "test");
-                expect($container.text()).toBe("Template content");
+                template($container, 'test');
+                expect($container.text()).toBe('Template content');
 
                 done();
             })
@@ -191,61 +247,3 @@ describe('DevExtreme Angular 2 widget', () => {
     });
 
 });
-
-//TODO: Try to replace dxButton to Widget ('require' required)
-var dxTestWidget = DevExpress.ui.dxButton["inherit"]({
-    NAME: "dxTestWidget",
-    _render(){
-        this.element()[0].classList.add("dx-test-widget");
-    }
-});
-
-DevExpress.registerComponent("dxTestWidget", dxTestWidget);
-
-@Component({
-    selector: 'dx-test-widget',
-    template:'',
-    inputs: ['testOption', 'testTemplate'],
-    outputs:['testOptionChange', "onOptionChanged", "testTemplateChange"],
-    providers: [
-        provide(DxTemplateHost, { useClass: DxTemplateHost })
-    ]
-})
-export class DxTestWidget extends DxComponent{
-    testOption: any;
-    testTemplate: any;
-    onOptionChanged: EventEmitter<any>;
-    testOptionChange: EventEmitter<any>; 
-    testTemplateChange: EventEmitter<any>; 
-
-    constructor(elementRef: ElementRef, ngZone: NgZone, templateHost: DxTemplateHost) {
-        super(elementRef, ngZone, templateHost);
-        this.widgetClassName = 'dxTestWidget';
-        this._events = [
-            { subscribe: 'optionChanged', emit: 'onOptionChanged' },
-            { subscribe: 'initialized', emit: 'onInitialized' }
-        ];
-
-        this._properties = [
-            'testOption',
-            'testTemplate'
-        ]
-
-        this.onOptionChanged = new EventEmitter();
-        this.testOptionChange = new EventEmitter();
-        this.testTemplateChange = new EventEmitter();
-    }
-}
-
-@Component({
-    selector: 'test-container-component',
-    template:'',
-    directives: [DxTestWidget, DxTemplate],
-    queries: {
-        innerWidgets: new ViewChildren(DxTestWidget)
-    }
-})
-export class TestContainerComponent {
-    constructor() {
-    }
-}
