50b6d6c84ff4a92c298c01af2922872f29a8271d
Aden-git <112aden358@gmail.com>
2016-09-14 11:47:58 +0300

    Update angular 2 version to 2.0.0-rc.6

--- examples/app/main.ts
+++ examples/app/main.ts
@@ -1,14 +1,64 @@
 import {NgModule, enableProdMode} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {AppComponent} from './app.component';
+import {
+    DxButtonModule,
+    DxCheckBoxModule,
+    DxSwitchModule,
+    DxTextBoxModule,
+    DxTextAreaModule,
+    DxNumberBoxModule,
+    DxDateBoxModule,
+    DxProgressBarModule,
+    DxSliderModule,
+    DxRangeSliderModule,
+    DxLoadIndicatorModule,
+    DxAutocompleteModule,
+    DxSelectBoxModule,
+    DxTagBoxModule,
+    DxRadioGroupModule,
+    DxColorBoxModule,
+    DxCalendarModule,
+    DxTemplateModule,
+    DxListModule,
+    DxPopupModule,
+    DxChartModule,
+    DxDataGridModule
+} from '../../dist';
 
 enableProdMode();
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    DxButtonModule,
+    DxCheckBoxModule,
+    DxSwitchModule,
+    DxTextBoxModule,
+    DxTextAreaModule,
+    DxNumberBoxModule,
+    DxDateBoxModule,
+    DxProgressBarModule,
+    DxSliderModule,
+    DxRangeSliderModule,
+    DxLoadIndicatorModule,
+    DxAutocompleteModule,
+    DxSelectBoxModule,
+    DxTagBoxModule,
+    DxRadioGroupModule,
+    DxColorBoxModule,
+    DxCalendarModule,
+    DxTemplateModule,
+    DxListModule,
+    DxPopupModule,
+    DxChartModule,
+    DxDataGridModule
+  ],
   bootstrap: [AppComponent],
 })
 class MyAppModule {}
--- examples/systemjs.config.js
+++ examples/systemjs.config.js
@@ -1,34 +1,42 @@
-var map = {
-    '@angular':                   '../node_modules/@angular',
-    'angular2-in-memory-web-api': '../node_modules/angular2-in-memory-web-api',
-    'rxjs':                       '../node_modules/rxjs',
-    '../../dist': '../../dist'
-};
-
-var packages = {
-    'app':                        { main: 'main.js',  defaultExtension: 'js' },
-    'rxjs':                       { defaultExtension: 'js' },
-    'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
-    '../../dist': { main: 'index.js', defaultExtension: 'js' },
-};
-
-var ngPackageNames = [
-    'common',
-    'compiler',
-    'core',
-    'forms',
-    'http',
-    'platform-browser',
-    'platform-browser-dynamic',
-];
-
-function packIndex(pkgName) {
-    packages['@angular/'+pkgName] = { main: 'index.js', defaultExtension: 'js' };
-}
-
-ngPackageNames.forEach(packIndex);
-
-System.config({
-    map: map,
-    packages: packages
-});
+(function (global) {
+  System.config({
+    paths: {
+      // paths serve as alias
+      'npm:': '../node_modules/'
+    },
+    map: {
+      // our app is within the app folder
+      app: 'app',
+      // angular bundles
+      '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
+      '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
+      '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
+      '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
+      '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+      '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
+      '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
+      '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+      // other libraries
+      'rxjs': 'npm:rxjs',
+      'angular2-in-memory-web-api': 'npm:angular2-in-memory-web-api',
+      '../../dist': '../../dist'
+    },
+    packages: {
+      app: {
+        main: './main.js',
+        defaultExtension: 'js'
+      },
+      rxjs: {
+        defaultExtension: 'js'
+      },
+      'angular2-in-memory-web-api': {
+        main: './index.js',
+        defaultExtension: 'js'
+      },
+      '../../dist': {
+        main: 'index.js',
+        defaultExtension: 'js'
+      }
+    }
+  });
+})(this);
