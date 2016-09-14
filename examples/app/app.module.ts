8842d8887da450e29ec9dd88b625a2a7f12c54f6
Aden-git <112aden358@gmail.com>
2016-09-14 13:37:03 +0300

    Format code

--- examples/app/app.module.ts
+++ examples/app/app.module.ts
@@ -1,7 +1,7 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {ReactiveFormsModule} from '@angular/forms';
-import {AppComponent} from './app.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
 import {
     DxButtonModule,
     DxCheckBoxModule,
--- examples/app/main.ts
+++ examples/app/main.ts
@@ -1,6 +1,6 @@
-import {enableProdMode} from '@angular/core';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {MyAppModule} from './app.module';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { MyAppModule } from './app.module';
 
 enableProdMode();
 
