624eef285cd1db6bc7b6023003efcb6e8a4aaa8d
bykov <dxbykov@users.noreply.github.com>
2016-12-05 17:47:28 +0300

    Add the project URL to the license header

--- gulpfile.js
+++ gulpfile.js
@@ -157,7 +157,9 @@ gulp.task('npm.license-headers', ['npm.content', 'npm.sources', 'npm.modules'], 
         ' * Copyright (c) 2012 - <%= year %> Developer Express Inc. ALL RIGHTS RESERVED',
         ' *',
         ' * This software may be modified and distributed under the terms',
-        ' * of the MIT license.  See the LICENSE file for details.',
+        ' * of the MIT license.  See the LICENSE file in the root of the project for details.',
+        ' *',
+        ' * https://github.com/DevExpress/devextreme-angular',
         ' */',
         ''
         ].join('\n');
