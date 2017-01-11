b2cf0a9f5b74860144ef9c203012680d0ea92ade
Yury Orlov <mpcteam.kvet@gmail.com>
2017-01-11 16:11:19 +0300

    Support validation rules defined by nested options

--- examples/app/app.component.ts
+++ examples/app/app.component.ts
@@ -99,20 +99,12 @@ import{
 export class AppComponent implements OnInit {
     @ViewChild(DxPopoverComponent) popover: DxPopoverComponent;
     text = 'Initial text';
-    email: string;
+    angularFormData = { email: '', password: '' };
     emailControl: AbstractControl;
-    password: string;
     passwordControl: AbstractControl;
-    dxValidationRules = {
-        email: [
-            { type: 'required', message: 'Email is required.' },
-            { type: 'email', message: 'Email is invalid.' }
-        ],
-        password: [
-            { type: 'required', message: 'Email is required.' }
-        ]
-    };
     form: FormGroup;
+    dxFormControlsData = { email: '', password: '' };
+    dxFormData = { email: '', password: '' };
     boolValue: boolean;
     numberValue: number;
     dateValue: Date;
