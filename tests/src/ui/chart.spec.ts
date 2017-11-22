60ec68f8db02057143f490d8f2544a525b2d4a92
Stanislav <112aden358@gmail.com>
2017-11-22 19:37:21 +0300

    Lock widget updating in time of the change detection cycle

--- tests/src/ui/chart.spec.ts
+++ tests/src/ui/chart.spec.ts
@@ -19,6 +19,14 @@ import {
 })
 class TestContainerComponent {
     @ViewChild(DxChartComponent) chart: DxChartComponent;
+    dataSource = [];
+    commonSeriesSettings = {
+        argumentField: undefined
+    };
+    seriesAsArray = [];
+    seriesAsObject = {
+        valueField: undefined
+    };
 }
 
 describe('DxChart', () => {
@@ -46,5 +54,65 @@ describe('DxChart', () => {
              element: any = chart.instance.element();
 
         expect(window.getComputedStyle(element).display).toBe('block');
+    });
+
+    it('should not repainting twice in change detection cycle after applying options directly', () => {
+        TestBed.overrideComponent(TestContainerComponent, {
+            set: {
+                template: `<dx-chart 
+                    [dataSource]="dataSource"
+                    [series]="seriesAsArray"
+                    [commonSeriesSettings]="{ argumentField: 'name' }"
+                ></dx-chart>`
+            }
+        });
+        let fixture = TestBed.createComponent(TestContainerComponent);
+        fixture.detectChanges();
+
+        let testComponent = fixture.componentInstance,
+            chart = testComponent.chart,
+            spy = spyOn(chart.instance, '_applyChanges');
+
+        testComponent.dataSource = [{
+            name: 1,
+            value: 2
+        }];
+        testComponent.seriesAsArray = [
+            { valueField: 'value'}
+        ];
+
+        fixture.detectChanges();
+
+        expect(spy.calls.count()).toBe(1);
+    });
+
+    it('should not repainting twice in change detection cycle after detect changes in arrays', () => {
+        TestBed.overrideComponent(TestContainerComponent, {
+            set: {
+                template: `<dx-chart 
+                    [dataSource]="dataSource"
+                    [series]="seriesAsArray"
+                    [commonSeriesSettings]="{ argumentField: 'name' }"
+                ></dx-chart>`
+            }
+        });
+        let fixture = TestBed.createComponent(TestContainerComponent);
+        fixture.detectChanges();
+
+        let testComponent = fixture.componentInstance,
+            chart = testComponent.chart,
+            spy = spyOn(chart.instance, '_applyChanges');
+
+        testComponent.dataSource.push({
+            name: 1,
+            value: 2
+        });
+        testComponent.seriesAsArray.push({
+            valueField: 'value'
+        });
+
+        fixture.detectChanges();
+
+        expect(spy.calls.count()).toBe(1);
     });
 });
