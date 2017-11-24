f4e667cc010453d9a827308866cb9fb84dc24216
volkov.vladislav <vladislav.volkov@devexpress.com>
2017-11-24 14:38:41 +0300

    Fix lint

--- src/core/component.ts
+++ src/core/component.ts
@@ -23,7 +23,7 @@ export abstract class DxComponent implements AfterViewInit,
     eventHelper: EmitterHelper;
     templates: DxTemplateDirective[];
     instance: any;
-    isLinked: boolean = true;
+    isLinked = true;
     changedOptions = {};
     renderOnViewInit = true;
 
