12b3b0670908c70561fdf3dc3204c774096dd99a
Andrey Ovchinnikov <ovchinnikov@users.noreply.github.com>
2018-02-16 11:56:42 +0300

    Use webpack to run widget tests (#692)
    * Use webpack to run widget tests
    
    * Try to fix tests

---
+++ webpack.test.js
@@ -0,0 +1,14 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+    devtool: 'inline-source-map',
+
+    plugins: [
+        new webpack.ContextReplacementPlugin(
+            /angular(\\|\/)core(\\|\/)(@angular|esm5)/,
+            path.join(__dirname, '../tests'),
+            {} 
+        )
+    ]
+};
