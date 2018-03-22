fe4d8a48403bb0903e2cf9a6165d210bddd4e298
volkov.vladislav <vladislav.volkov@devexpress.com>
2018-03-22 17:55:05 +0300

    Implement ajax request caching

---
+++ tests/src/server/ssr-ajax.spec.ts
@@ -0,0 +1,93 @@
+/* tslint:disable:component-selector */
+
+import { Component, PLATFORM_ID } from '@angular/core';
+
+import { isPlatformServer } from '@angular/common';
+
+import * as def from 'devextreme/core/utils/deferred';
+import * as ajax from 'devextreme/core/utils/ajax';
+
+import { ServerModule, ServerTransferStateModule } from '@angular/platform-server';
+import { BrowserModule, BrowserTransferStateModule, TransferState, makeStateKey } from '@angular/platform-browser';
+
+import { NgHttp } from '../../../dist';
+
+import {
+    TestBed,
+    async
+} from '@angular/core/testing';
+
+let mockSendRequest = {
+    callBase: function() {
+        let d = new def.Deferred(),
+            promise = d.promise();
+        d.resolve('test', 'success');
+        return promise;
+    }
+};
+
+@Component({
+    selector: 'test-container-component',
+    template: ''
+})
+class TestContainerComponent {
+}
+
+describe('Universal', () => {
+    let sendRequest: any;
+    let ajaxInject = ajax.inject;
+    beforeEach(() => {
+        ajax.inject = function(obj) {
+            sendRequest = obj['sendRequest'];
+
+        };
+        TestBed.configureTestingModule(
+            {
+                declarations: [TestContainerComponent],
+                imports: [ServerModule,
+                    ServerTransferStateModule,
+                    BrowserTransferStateModule,
+                    BrowserModule.withServerTransition({appId: 'appid'})],
+                providers: [NgHttp]
+            });
+    });
+
+    afterEach(function() {
+        ajax.inject = ajaxInject;
+    });
+    // spec
+    it('should set state and remove data from the state when the request is repeated', async(() => {
+        const platformId = TestBed.get(PLATFORM_ID);
+        if (isPlatformServer(platformId)) {
+            TestBed.get(NgHttp);
+
+            sendRequest.apply(mockSendRequest, [{url: 'someurl'}]);
+            let key = makeStateKey('0urlsomeurl');
+            const transferState: TransferState = TestBed.get(TransferState);
+
+
+            expect(transferState.hasKey(key)).toBe(true);
+            expect(transferState.get(key, null as any)).toEqual(Object({ data: 'test', status: 'success' }));
+
+            sendRequest.apply(mockSendRequest, [{url: 'someurl'}]);
+
+            expect(transferState.hasKey(key)).toBe(true);
+            expect(transferState.get(key, null as any)).toEqual(null);
+        }
+    }));
+
+    it('should generate complex key', async(() => {
+        const platformId = TestBed.get(PLATFORM_ID);
+        if (isPlatformServer(platformId)) {
+            TestBed.get(NgHttp);
+
+
+            sendRequest.apply(mockSendRequest, [{url: 'someurl', data: { filter: { name: 'test'}, select: ['name']}}]);
+            let key = makeStateKey('0urlsomeurldatafilternametestselect0name');
+            const transferState: TransferState = TestBed.get(TransferState);
+
+            expect(transferState.hasKey(key)).toBe(true);
+        }
+    }));
+
+});
