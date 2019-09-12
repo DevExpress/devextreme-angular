868f031f3c5ff93becb71ae7d06d46db39645102
Andrey Ovchinnikov <ovchinnikov@users.noreply.github.com>
2019-09-12 12:02:19 +0300

    Support Angular 9 (#992)

--- src/core/component.ts
+++ src/core/component.ts
@@ -14,7 +14,6 @@ import {
     AfterViewInit
 } from '@angular/core';
 
-import { ɵgetDOM as getDOM } from '@angular/platform-browser';
 import { isPlatformServer } from '@angular/common';
 import { TransferState, makeStateKey } from '@angular/platform-browser';
 
@@ -22,7 +21,9 @@ import { DxTemplateDirective } from './template';
 import { IDxTemplateHost, DxTemplateHost } from './template-host';
 import { EmitterHelper, NgEventsStrategy } from './events-strategy';
 import { WatcherHelper } from './watcher-helper';
+import * as domAdapter from 'devextreme/core/dom_adapter';
 import * as events from 'devextreme/events';
+
 import {
     INestedOptionContainer,
     ICollectionNestedOption,
@@ -171,7 +172,7 @@ export abstract class DxComponent implements OnChanges, OnInit, DoCheck, AfterCo
             let element = this.instance.element();
             events.triggerHandler(element, 'dxremove', { _angularIntegration: true });
             this.instance.dispose();
-            getDOM().remove(element);
+            domAdapter.removeElement(element);
         }
     }
 
