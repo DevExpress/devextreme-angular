611bd5366720eef5ce8fba8b72d6ca0b41e1679a
ovchinnikov.andrey <ovchinnikov@users.noreply.github.com>
2019-10-21 13:02:35 +0300

    Revert unnecessary changes

--- tools/src/dot-generator.ts
+++ tools/src/dot-generator.ts
@@ -6,16 +6,16 @@ let doT = require('dot');
 
 doT.templateSettings = {
   evaluate:    /\<#([\s\S]+?)#\>/g,
-    interpolate: /\<#=([\s\S]+?)#\>/g,
+  interpolate: /\<#=([\s\S]+?)#\>/g,
   encode:      /\<#!([\s\S]+?)#\>/g,
   use:         /\<##([\s\S]+?)#\>/g,
   define:      /\<###\s*([\w\.$]+)\s*(\:|=)([\s\S]+?)##\>/g,
-    conditional: /\<#\?(\?)?\s*([\s\S]*?)\s*#\>/g,
+  conditional: /\<#\?(\?)?\s*([\s\S]*?)\s*#\>/g,
   iterate:     /\<#~\s*(?:#\>|([\s\S]+?)\s*\:\s*([\w$]+)\s*(?:\:\s*([\w$]+))?\s*#\>)/g,
-    varname: 'it',
-    strip: false,
-    append: true,
-    selfcontained: false
+  varname: 'it',
+  strip: false,
+  append: true,
+  selfcontained: false
 };
 
 export default class DoTGenerator {
