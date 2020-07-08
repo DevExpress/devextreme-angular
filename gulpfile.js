1fcf32285b7d05c4ce3cca72076d58d76a223232
OlegKipchatov <fenix667@yandex.ru>
2020-07-08 17:24:53 +0300

    Add run injector before npm pack

--- gulpfile.js
+++ gulpfile.js
@@ -148,7 +148,11 @@ gulp.task('npm.content', gulp.series('build.components', function() {
         .pipe(gulp.dest(npmConfig.distPath));
 }));
 
-gulp.task('npm.pack', gulp.series('npm.content', shell.task(['npm pack'], { cwd: buildConfig.npm.distPath })));
+gulp.task('npm.pack', gulp.series(
+    shell.task('npm run injector'),
+    'npm.content',
+    shell.task(['npm pack'], { cwd: buildConfig.npm.distPath })
+));
 
 //------------TSLint------------
 
