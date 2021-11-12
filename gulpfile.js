49829398792b41c315b0a1855129307c8bace737
Alexey Serov <alexserov@users.noreply.github.com>
2021-11-12 17:21:02 +0300

    Update versions to 22.1 (#1217)

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,14 @@ var tslint = require('gulp-tslint');
 const srcFilesPattern = ['packages/*/src/**/*.ts', '!packages/*/src/ui/**', '!packages/*/src/metadata-model.ts'];
 const sandboxSrcFilesPattern = 'packages/sandbox/**/*.ts';
 const testsFilesPattern = ['packages/*/tests/src/**/*.spec.ts', 'packages/*/tests/src/**/component-names.ts'];
+const nodeModulesExcludePattern = ['!**/node_modules/**/*'];
 
 
 gulp.task('lint', function() {
     return gulp.src(srcFilesPattern
             .concat(sandboxSrcFilesPattern)
             .concat(testsFilesPattern)
+            .concat(nodeModulesExcludePattern)
         )
         .pipe(tslint({
             formatter: 'prose',
